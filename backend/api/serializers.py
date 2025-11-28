cf86b52b79c559248553b47e73fcf132b230bb55
Joaquin54 <joaquinfrangibusiness@gmail.com>
2025-11-27 20:08:11 -0500

    fixed imports to make it more readable

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,15 +1,25 @@
-from rest_framework import fields, serializers
+from rest_framework import serializers
 from models import TriedRecipe, User, User_Profile
-from models_mongo import Ingredient, RecipeIngredientEmbedded, RecipeInstructionEmbedded, Recipe, SavedRecipe
+from models_mongo import (
+    Ingredient,
+    RecipeIngredientEmbedded,
+    RecipeInstructionEmbedded,
+    Recipe,
+    SavedRecipe,
+)
 
 # For MongoDB models (Recipe, Ingredient, SavedRecipe)
-from rest_framework_mongoengine import serializers as mongo_serializers
+from rest_framework_mongoengine.serializers import (
+    DocumentSerializer,
+    EmbeddedDocumentSerializer,
+)
 
 
 class UserSerializer(serializers.ModelSerializer):
     """
     Serialize user data from the User Model (model.py)
     """
+
     class Meta:
         model = User
         fields = [
@@ -20,195 +30,209 @@ class UserSerializer(serializers.ModelSerializer):
             "diet_type",
             "date_created",
         ]
-
-        read_only_fields = ['public_id', 'date_created']
+        read_only_fields = ["public_id", "date_created"]
 
     def validate_username(self, value):
-        if len(value < 3):
+        # length checks
+        if len(value) < 4:
             raise serializers.ValidationError(
-                "Username must be longer than 4 characters")
-        if len(value > 24):
+                "Username must be longer than 4 characters"
+            )
+        if len(value) > 24:
             raise serializers.ValidationError(
-                "Username must be 24 characters or shorter")
+                "Username must be 24 characters or shorter"
+            )
 
+        # uniqueness
         if User.objects.filter(username=value).exists():
             raise serializers.ValidationError("Username already exists")
 
+        # forbidden names
         forbidden_words = ["admin", "root", "system", "nutrichoice"]
         if value.lower() in forbidden_words:
             raise serializers.ValidationError("Username not allowed")
 
+        return value
+
+
 class TriedRecipeSerializer(serializers.ModelSerializer):
-    model = TriedRecipe
-    fields = [
-        "public_id",
-        "date_added",
-        "tried_by",
-    ]
+    class Meta:
+        model = TriedRecipe
+        fields = [
+            "public_id",
+            "date_added",
+            "tried_by",
+        ]
+        read_only_fields = ["public_id", "date_added"]
 
-    read_only_fields = ['public_id', 'date_added']
+    # Object-level validation
+    # See https://www.django-rest-framework.org/api-guide/serializers/#object-level-validation
+    def validate(self, data):
+        public_id = data["public_id"]
+        tried_by = data["tried_by"]
 
+        if TriedRecipe.objects.filter(public_id=public_id, tried_by=tried_by).exists():
+            raise serializers.ValidationError(
+                "Recipe already tried by this user"
+            )
 
-    #Object level validation, data is dict which holds all fields for a given model
-    #See https://www.django-rest-framework.org/api-guide/serializers/#object-level-validation
-    def valide(self, data):
-        public_id = data['public_id']
-        tried_by = data['tried_by']
+        return data
 
-        if(TriedRecipe.objects.filter(public_id=public_id, tried_by=tried_by)):
-            raise serializers.ValidationError("Recipe already tried by user this user")
-
-
-#    def validate_triedrecipe(self, value):
-#        if TriedRecipe.objects.filter(public_id=public_id, tried_by=tried_by).exists():
-#            raise serializers.ValidationError(
-#                "Recipe already tried by user this user")
-        
-
-#All serializers below added by Pedro (hopefully they work)
 
 class UserProfileSerializers(serializers.ModelSerializer):
     class Meta:
-        #Postgree model user profile
+        # Postgres model user profile
         model = User_Profile
-        #Data fields of the postgree table/model
+        # Data fields of the postgres table/model
         fields = [
-            "id", 
-            "user", 
-            "daily_calorie_goal", 
+            "id",
+            "user",
+            "daily_calorie_goal",
             "daily_protein_goal",
             "date_created",
             "date_updated",
             "bio",
             "diet_type",
-            "profil_picture" #Typo here is copied from model
-                  ]
-        
+            "profil_picture",  # Typo here is copied from model
+        ]
         read_only_fields = ["id", "date_created"]
 
-        def validate_id(self, value):
-            if User_Profile.objects.filter(id=value).exists():
-                raise serializers.ValidationError(
-                    "Another user in the system has identical ID"
-                )
-            
-        #Ensure one user does not have multiple profiles
-        def validate_user(self, value):
-            if User_Profile.objects.filter(user=value).exists():
-                raise serializers.ValidationError(
-                    "User already has a profile created"
-                )
+    def validate_id(self, value):
+        if User_Profile.objects.filter(id=value).exists():
+            raise serializers.ValidationError(
+                "Another user in the system has identical ID"
+            )
+        return value
+
+    # Ensure one user does not have multiple profiles
+    def validate_user(self, value):
+        if User_Profile.objects.filter(user=value).exists():
+            raise serializers.ValidationError(
+                "User already has a profile created"
+            )
+        return value
 
 
-
-class IngredientSerializer(mongo_serializers.DocumentSerializer):
-    class meta:
+class IngredientSerializer(DocumentSerializer):
+    class Meta:
         model = Ingredient
-        fields = ["public_id", 
-                "name",
-                "category",
-                "calories",
-                "protein"
-                "carbs",
-                "fat",
-                "fiber",
-                "sugar",
-                "sodium",
-                "conversions",
-                "created_date",
-                "updated_date"
-                ]
-    
+        fields = [
+            "public_id",
+            "name",
+            "category",
+            "calories",
+            "protein",
+            "carbs",
+            "fat",
+            "fiber",
+            "sugar",
+            "sodium",
+            "conversions",
+            "created_date",
+            "updated_date",
+        ]
         read_only_fields = ["public_id", "created_date", "updated_date"]
 
-#Not sure what this serializer accomplishes
-class IngredientListSerializer(mongo_serializers.DocumentSerializer):
 
-    class meta:
+# Not sure what this serializer accomplishes
+class IngredientListSerializer(DocumentSerializer):
+    class Meta:
         model = Ingredient
-        fields = ["public_id",
-                  "name",
-                  "category",
-                  "calories",
-                  "protein",
-                  "carbs",
-                  "fat"
-                  ]
-        read_only_fields = ["public_id",]
+        fields = [
+            "public_id",
+            "name",
+            "category",
+            "calories",
+            "protein",
+            "carbs",
+            "fat",
+        ]
+        read_only_fields = ["public_id"]
 
-class RecipeIngredientEmbeddedSerializer(mongo_serializers.EmbeddedDocumentSerializer):
-    class meta:
+
+class RecipeIngredientEmbeddedSerializer(EmbeddedDocumentSerializer):
+    class Meta:
         model = RecipeIngredientEmbedded
-        fields = ["ingredient_id",
-                  "ingredient_name",
-                  "quantity_grams",
-                  "display_quantity",
-                  "display_unit",
-                  "preparation_notes",
-                  "order"
-                  ]
-        
-class RecipeInstructionEmbeddedSerializer(mongo_serializers.EmbeddedDocumentSerializer):
-    class meta:
+        fields = [
+            "ingredient_id",
+            "ingredient_name",
+            "quantity_grams",
+            "display_quantity",
+            "display_unit",
+            "preparation_notes",
+            "order",
+        ]
+
+
+class RecipeInstructionEmbeddedSerializer(EmbeddedDocumentSerializer):
+    class Meta:
         model = RecipeInstructionEmbedded
-        fields = ["step_number",
-                  "instruction",
-                  "duration_minutes"]
-        
+        fields = [
+            "step_number",
+            "instruction",
+            "duration_minutes",
+        ]
 
-class RecipeSerializer(mongo_serializers.DocumentSerializer):
 
-    #nested serialization
+class RecipeSerializer(DocumentSerializer):
+    # nested serialization
     ingredients = RecipeIngredientEmbeddedSerializer(many=True)
     instructions = RecipeInstructionEmbeddedSerializer(many=True)
 
-    class meta:
+    class Meta:
         model = Recipe
-        fields = ["public_id",
-                  "user_id",
-                  "title",
-                  "description",
-                  "image_url",
-                  "prep_time",
-                  "cook_time",
-                  "ingredients", #nested
-                  "instructions", #nested
-                  "nutrition_per_serving",
-                  "nutrition_total",
-                  "cuisine_type",
-                  "dietary_tags",
-                  "is_public",
-                  "date_time_created",
-                  "date_time_updated"]
-        
-        read_only_fields = ["public_id", "nutrition_per_serving", "nutrition_total", 
-                            "date_time_created", "date_time_updated"]
+        fields = [
+            "public_id",
+            "user_id",
+            "title",
+            "description",
+            "image_url",
+            "prep_time",
+            "cook_time",
+            "ingredients",  # nested
+            "instructions",  # nested
+            "nutrition_per_serving",
+            "nutrition_total",
+            "cuisine_type",
+            "dietary_tags",
+            "is_public",
+            "date_time_created",
+            "date_time_updated",
+        ]
+        read_only_fields = [
+            "public_id",
+            "nutrition_per_serving",
+            "nutrition_total",
+            "date_time_created",
+            "date_time_updated",
+        ]
 
-class RecipeListSerializer:
 
-    class meta:
+class RecipeListSerializer(DocumentSerializer):
+    class Meta:
         model = Recipe
-        fields = ["public_id",
-                  "title",
-                  "description",
-                  "image_url",
-                  "prep_time",
-                  "cook_time",
-                  "cuisine_type",
-                  "nutrition_per_serving",
-                  "is_public"]
-        
+        fields = [
+            "public_id",
+            "title",
+            "description",
+            "image_url",
+            "prep_time",
+            "cook_time",
+            "cuisine_type",
+            "nutrition_per_serving",
+            "is_public",
+        ]
         read_only_fields = ["public_id", "nutrition_per_serving"]
 
 
-class SavedRecipeSerializer:
-
-    class meta:
+class SavedRecipeSerializer(DocumentSerializer):
+    class Meta:
         model = SavedRecipe
-        fields = ["public_id",
-                  "user_id",
-                  "recipe_id",
-                  "notes",
-                  "saved_date"]
-        
+        fields = [
+            "public_id",
+            "user_id",
+            "recipe_id",
+            "notes",
+            "saved_date",
+        ]
         read_only_fields = ["public_id", "saved_date"]
