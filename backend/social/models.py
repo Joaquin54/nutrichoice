1a9817655e1a06c4252b4ebdc3da51117cc78818
Joaquin54 <joaquinfrangibusiness@gmail.com>
2026-02-05 09:47:50 -0500

    feat: added UserFollows model, for user following

--- backend/social/models.py
+++ backend/social/models.py
@@ -1,8 +1,42 @@
 from django.db import models
 from uuid import uuid4
 from users.models import User
+from django.conf import settings
+from django.db.models import Q, F
 
 # Create your models here.
+
+
+class UserFowllows(models.Model):
+    follower = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        relatedi_name="following_edges"
+    )
+    followee = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="follower_edges"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "user_follows"
+
+        # Prevent duplicates: one follow edge per (follower, followee)
+        constraints = [
+            models.UniqueConstraint(
+                fields=["follower", "followee"],
+                name="uq_userfollow_follower_followee"
+            ),
+            # Prevent self-follow
+            models.CheckConstraint(
+                check=~Q(follower=F("followee")),
+                name="ck_userfollow_no_self_follow",
+            )
+        ]
+
+
 class TriedRecipe(models.Model):
     """
     Tracks recipes that users have tried.
@@ -22,7 +56,8 @@ class TriedRecipe(models.Model):
     )
 
     def __str__(self):
-        return f"{self.public_id} recipe tried by {self.tried_by.username}" # type: ignore
+        # type: ignore
+        return f"{self.public_id} recipe tried by {self.tried_by.username}"
 
     class Meta:
         unique_together = ('tried_by', 'recipe_id')
