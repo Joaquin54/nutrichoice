709ead9b5a664329f20bd9698c9dd9e1cff00562
Joaquin54 <joaquinfrangibusiness@gmail.com>
2026-03-10 21:04:38 -0400

    fix: fixed formatting

--- backend/api/views/nutrition.py
+++ backend/api/views/nutrition.py
@@ -22,16 +22,16 @@ class RecipeNutritionView(APIView):
                 {"error": "Recipe not found."},
                 status=status.HTTP_404_NOT_FOUND
             )
-    
+
         nutrition = self.calculateNutritionPer100g(recipe)
 
-        if "error" in  nutrition:
+        if "error" in nutrition:
             return Response(nutrition, status=status.HTTP_404_NOT_FOUND)
-        
+
         return Response(nutrition, status=status.HTTP_200_OK)
 
     def calculateNutritionPer100g(self, recipe):
-        STD_Q = "_per_100g" # Based of Ingredients/models.py
+        STD_Q = "_per_100g"  # Based of Ingredients/models.py
         NUTRIENTS = [
             "calories" + STD_Q,
             "protein" + STD_Q,
@@ -46,33 +46,31 @@ class RecipeNutritionView(APIView):
         totals = {nutrient: Decimal("0.00") for nutrient in NUTRIENTS}
         recipe_weight = Decimal("0.00")
 
-        #Query set data type
-        #2 recipes
-        #borger - meat, cheese, bread
-        #chicken sandwich - chicken, lettuce, bread
+        # Query set data type
+        # 2 recipes
+        # borger - meat, cheese, bread
+        # chicken sandwich - chicken, lettuce, bread
 
-        # 1. borger - meat 
-        # 2. borger- cheese 
-        # 3. borger - bread 
-        # 4. Chicken sandwich - chicken 
-        # 5. Chicken sandwich - lettuce 
+        # 1. borger - meat
+        # 2. borger- cheese
+        # 3. borger - bread
+        # 4. Chicken sandwich - chicken
+        # 5. Chicken sandwich - lettuce
         # 6. Chicken sandwich - bread
 
-        #Final set
-        # 1. borger - meat 
-        # 2. borger- cheese 
-        # 3. borger - bread 
+        # Final set
+        # 1. borger - meat
+        # 2. borger- cheese
+        # 3. borger - bread
 
-
-        
-        temp = RecipeIngredient.objects.all().filter(recipe = self)
+        temp = RecipeIngredient.objects.all().filter(recipe=self)
 
         RecipeIngredient_set = temp.values("ingredient", "quantity", "unit")
         # RecipeIngredient_set  = [{ingredient: ingredient1, quantity: qty1, unit: unitQty1}]
         # List of dictionaries
 
-        #List of quantity of each ingredient
-        ingredient_qty_set = {} # {ingredient: grams}
+        # List of quantity of each ingredient
+        ingredient_qty_set = {}  # {ingredient: grams}
 
         for eachRecipeIngredient in RecipeIngredient_set:
             name = eachRecipeIngredient["ingredient"]
@@ -82,8 +80,9 @@ class RecipeNutritionView(APIView):
 
         # Very inefficient, rewrite later -> Would be much easier with sql style joins -> do they exist in django?
         for eachRecipeIngredient in RecipeIngredient_set:
-            #This retrieves the actual ingredient with all its nutrient qts
-            ingredient = Ingredient.objects.all().filter(ingredient = eachRecipeIngredient["ingredient"]).values()
+            # This retrieves the actual ingredient with all its nutrient qts
+            ingredient = Ingredient.objects.all().filter(
+                ingredient=eachRecipeIngredient["ingredient"]).values()
             for Nutrient in NUTRIENTS:
 
                 '''
@@ -96,17 +95,9 @@ class RecipeNutritionView(APIView):
                 ingredient_qty_in_org_recipe = ingredient_qty_set[ingredient]
                 ingredient_std = ingredient_qty_in_org_recipe / 100
 
-                nutrient_in_ingredient_standardized_for_recipe = ingredient_std * nutrient_in_ingredient_per_100g
+                nutrient_in_ingredient_standardized_for_recipe = ingredient_std * \
+                    nutrient_in_ingredient_per_100g
 
                 totals[Nutrient] += nutrient_in_ingredient_standardized_for_recipe
 
         return totals
-
-            
-
-            
-            
-            
-
-
-
